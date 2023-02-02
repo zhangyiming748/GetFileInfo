05a746dc3ba4f43ea3e596de994ed2620c3f5a60
zen <zhangyiming748@gmail.com>
2023-02-02 21:34:05 +0800

    create

---
+++ GetFileInfo.go
@@ -0,0 +1,29 @@
+package GetFileInfo
+
+/*
+获取单个文件信息
+*/
+func GetFileInfo() {
+
+}
+
+/*
+获取目录下符合条件的所有文件信息
+*/
+func GetAllFileInfo() {
+
+}
+
+/*
+获取单个视频文件信息
+*/
+func GetVideoFileInfo() {
+
+}
+
+/*
+获取目录下符合条件的所有视频文件信息
+*/
+func GetAllVideoFileInfo() {
+
+}
