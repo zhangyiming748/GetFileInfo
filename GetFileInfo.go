6a2a46b297bc40fb489e6c6bd5f468dc4fed3fb5
zen <zhangyiming748@gmail.com>
2023-02-02 21:47:53 +0800

    决定入参

--- GetFileInfo.go
+++ GetFileInfo.go
@@ -3,27 +3,27 @@ package GetFileInfo
 /*
 获取单个文件信息
 */
-func GetFileInfo() {
+func GetFileInfo(path string) {
 
 }
 
 /*
 获取目录下符合条件的所有文件信息
 */
-func GetAllFileInfo() {
+func GetAllFileInfo(dir, pattern string) {
 
 }
 
 /*
 获取单个视频文件信息
 */
-func GetVideoFileInfo() {
+func GetVideoFileInfo(path string) {
 
 }
 
 /*
 获取目录下符合条件的所有视频文件信息
 */
-func GetAllVideoFileInfo() {
+func GetAllVideoFileInfo(dir, pattern string) {
 
 }
